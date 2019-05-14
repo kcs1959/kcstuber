583e4dd4c931f92c5d84ab95a0528d3a909f2a3d
Lait-au-Cafe <laitaucafe8@gmail.com>
2019-05-15 05:23:16 +0900

    Implement animation that we see at many web pages.

--- docs/js/main.js
+++ docs/js/main.js
@@ -10,4 +10,16 @@
     });
 
     $('#top_content').addClass("fade");
+
+    const effect_thresh = 0.1;  
+    $(window).scroll(function(){
+        $('.effect').each(function(){
+            let elem_pos = $(this).offset().top;
+            let scroll_pos = $(window).scrollTop();
+            let window_height = $(window).height();
+            if(elem_pos < scroll_pos + window_height * (1 - effect_thresh)){
+                $(this).addClass('target');
+            }
+        });
+    });
 })();
